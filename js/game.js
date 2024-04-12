7dcd32dbef9d90da8e14d9d2f98e0630c45063fd
Kendrazapfen <doerte.sonnenstuhl@gmx.de>
2024-04-12 11:38:56 +0200

    error in console

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 let canvas;
-let ctx;
-let character = new Character();
-let enemies = [new Chicken(), new Chicken(), new Chicken()];
+let world = new World();
+
 
 function init() {
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+  canvas = document.getElementById('canvas');
+  world = new World(canvas);
+  
 
-  console.log("my character ist", character);
+  console.log("my character ist", world.character);
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,13 +1,18 @@
 class MoveableObject {
   x = 120;
-  y = 400;
+  y = 250;
   img;
+  height = 150;
+  width = 150;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
   moveRight() {
     console.log("Moving right");
   }
 
-  moveLeft(){
-        
-  }
+  moveLeft() {}
 }

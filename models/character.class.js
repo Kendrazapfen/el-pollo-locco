be0513a672916a30b51fd35ebe95da365f8ad092
Kendrazapfen <doerte.sonnenstuhl@gmx.de>
2024-04-23 22:59:54 +0200

    JSON for images

--- models/character.class.js
+++ models/character.class.js
@@ -1,13 +1,19 @@
-class Character extends MoveableObject{
-    y=130;
-    width= 300;
-    height=150;
+class Character extends MoveableObject {
+  y = 130;
+  width = 300;
+  height = 150;
 
-    constructor(){
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-    }
+  constructor() {
+    super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+    this.loadImages([
+      'img/2_character_pepe/2_walk/W-21.png',
+      'img/2_character_pepe/2_walk/W-22.png',
+      'img/2_character_pepe/2_walk/W-23.png',
+      'img/2_character_pepe/2_walk/W-24.png',
+      'img/2_character_pepe/2_walk/W-25.png',
+      'img/2_character_pepe/2_walk/W-26.png',
+    ]);
+  }
 
-    jump(){
-
-    }
+  jump() {}
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -2,12 +2,22 @@ class MoveableObject {
   x = 120;
   y = 280;
   img;
-  width = 150;
+  width = 100;
   height = 150;
+  imageCache = [];
   
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  loadImages(arr){
+    arr.forEach((path)=>{
+      let img = new Image();
+    img.src = path;
+    this.imageCache[path]=path;
+    });
+    
   }
 
   moveRight() {

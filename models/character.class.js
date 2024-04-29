75a50c66d1f851c722a6e419f0f4463413a4aa6e
Kendrazapfen <doerte.sonnenstuhl@gmx.de>
2024-04-29 14:46:32 +0200

    new images jumping

--- models/character.class.js
+++ models/character.class.js
@@ -10,6 +10,19 @@ class Character extends MoveableObject {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png',
   ];
+
+  IMAGES_JUMPING = [
+    'img/2_character_pepe/3_jump/J-31.png',
+    'img/2_character_pepe/3_jump/J-32.png',
+    'img/2_character_pepe/3_jump/J-33.png',
+    'img/2_character_pepe/3_jump/J-34.png',
+    'img/2_character_pepe/3_jump/J-35.png',
+    'img/2_character_pepe/3_jump/J-36.png',
+    'img/2_character_pepe/3_jump/J-37.png',
+    'img/2_character_pepe/3_jump/J-38.png',
+    'img/2_character_pepe/3_jump/J-39.png',
+  ];
+
   world;
   speed = 10;
   walking_sound = new Audio('audio/walking.mp3');
@@ -17,6 +30,7 @@ class Character extends MoveableObject {
   constructor() {
     super().loadImage(this.IMAGES_WALKING[0]);
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravity();
     this.animate();
   }
@@ -26,23 +40,25 @@ class Character extends MoveableObject {
       this.walking_sound.pause();
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.x += this.speed;
-        this.otherDirection=false;
+        this.otherDirection = false;
         this.walking_sound.play();
       }
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.LEFT && this.x >0) {
+      if (this.world.keyboard.LEFT && this.x > 0) {
         this.x -= this.speed;
-        this.otherDirection=true;
+        this.otherDirection = true;
         this.walking_sound.play();
       }
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
     setInterval(() => {
+      if(this.isAboveGround()){
+        this.playAnimation(this.IMAGES_JUMPING);
+      }else{}
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-       
         this.playAnimation(this.IMAGES_WALKING);
       }
     }, 50);

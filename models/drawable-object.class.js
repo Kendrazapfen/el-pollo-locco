2c935f94ded524a576e8eefe65130cf5f5afb3af
Kendrazapfen <doerte.sonnenstuhl@gmx.de>
2024-05-02 21:56:59 +0200

    refactoring moveable-object

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,1 +1,27 @@
-class DrawableObject {}
+class DrawableObject {
+  img;
+  imageCache = [];
+  currentImage = 0;
+  x = 120;
+  y = 280;
+  width = 100;
+  height = 150;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      img.style = 'transform: scaleX(-1)';
+      this.imageCache[path] = img;
+    });
+  }
+}
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,11 +1,4 @@
 class MoveableObject extends DrawableObject {
-  x = 120;
-  y = 280;
-  img;
-  width = 100;
-  height = 150;
-  imageCache = [];
-  currentImage = 0;
   speed = 0.15;
   otherDirection = false;
   speedY = 0;
@@ -13,20 +6,7 @@ class MoveableObject extends DrawableObject {
   energy = 100;
   lastHit = 0;
 
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      img.style = "transform: scaleX(-1)";
-      this.imageCache[path] = img;
-    });
-  }
-
+  
   moveRight() {
     this.x += this.speed;
   }
@@ -59,10 +39,6 @@ class MoveableObject extends DrawableObject {
     this.speedY = 30;
   }
 
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
-
   drawFrame(ctx) {
     if (
       this instanceof Character ||
@@ -70,8 +46,8 @@ class MoveableObject extends DrawableObject {
       this instanceof SmallChicken
     ) {
       ctx.beginPath();
-      ctx.lineWidth = "5";
-      ctx.strokeStyle = "blue";
+      ctx.lineWidth = '5';
+      ctx.strokeStyle = 'blue';
       ctx.rect(this.x, this.y, this.width, this.height);
       ctx.stroke();
     }
